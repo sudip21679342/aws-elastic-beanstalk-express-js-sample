aa10a6e1d68b95a83eca87b7d17504679c5648fc
sudip21679342 <144770706+sudip21679342@users.noreply.github.com>
2024-10-26 15:36:59 +0800

    Update app.js
    bug fixes !

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My Student ID is 21679342. Welcome to Express !'));
+app.get('/', (req, res) => {
+  res.send('My Student ID is 21679342. Welcome to Express!');
+});
 
 app.listen(port);
-console.log(`App running on http://localhost:${port}`);
